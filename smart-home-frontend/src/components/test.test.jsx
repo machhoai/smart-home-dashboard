b75e58e5775c72c3ce96829ca1c60503f049ab76
HoaiPC <machhoai1234@gmail.com>
2025-09-27 01:51:22 +0700

    Update Test component to use dynamic viewport height for improved layout

--- smart-home-frontend/src/components/test.test.jsx
+++ smart-home-frontend/src/components/test.test.jsx
@@ -35,7 +35,8 @@ const Test = () => {
     }, []);
 
     return (
-        <div className=" text-white w-screen h-screen p-3 relative font-normal"
+        <div className=" text-white w-screen p-3 relative font-normal"
+            style={{ height: "100dvh" }}
         // style={{ backgroundImage: "url('/milky-way-starry-sky-night-mountains-lake-reflection-cold-5k-4480x2520-287.jpg')", backgroundSize: "cover", backgroundPosition: "center" }}
         >
             {/* <div className="fixedMsg fixed top-7 right-1/2 translate-x-1/2 bg-[#121212] w-60 rounded-full flex px-3 py-1 gap-20"
