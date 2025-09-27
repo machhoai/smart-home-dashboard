bdde72e2f4d1791119ccf4b12bf61557d6b73ba6
HoaiPC <machhoai1234@gmail.com>
2025-09-28 00:08:50 +0700

    Add additional CORS origin for improved frontend access

--- smart-home-backend/server.js
+++ smart-home-backend/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 // );
 
 app.use(cors({
-    origin: ["https://hoai.homes"], // hoặc mảng domain frontend
+    origin: ["https://hoai.homes", "https://www.hoai.homes"], // hoặc mảng domain frontend
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
